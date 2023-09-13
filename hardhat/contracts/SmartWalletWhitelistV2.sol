314170d9573f18407288d58c149b1222b1564435
thanhtrinh2003 <thanhtrinh280506@gmail.com>
2023-09-13 15:51:07 -0400

    update superadmin role related requirement

--- hardhat/contracts/SmartWalletWhitelistV2.sol
+++ hardhat/contracts/SmartWalletWhitelistV2.sol
@@ -29,13 +29,13 @@ contract SmartWalletWhitelistV2 {
     }
     
     function commitSetChecker(address _checker) external {
-        require(isAdmin(msg.sender), "!admin");
+        require(isSuperAdmin(msg.sender), "Error: Not Super admin");
         future_checker = _checker;
     }
 
     function changeAdmin(address _admin, bool validAdmin) external {
-        require(isAdmin(msg.sender), "!admin");
-        require(!isSuperAdmin(_admin), "!auth"); // Overwrite protection
+        require(isSuperAdmin(msg.sender), "Error: Not Super admin");
+        require(!isSuperAdmin(_admin), "Error: Entered address is already Super admin"); // Overwrite protection
         if(validAdmin) {
             roles[_admin] = ADMIN;
         } else {
@@ -44,25 +44,25 @@ contract SmartWalletWhitelistV2 {
     }
 
     function transferSuperAdmin(address _newAdmin) external {
-        require(isSuperAdmin(msg.sender),'!sAdmin');
+        require(isSuperAdmin(msg.sender),"Error: Not Super admin");
         roles[msg.sender] = 0x0;
         roles[_newAdmin] = SUPER_ADMIN;
     }
     
     function applySetChecker() external {
-        require(isAdmin(msg.sender), "!admin");
+        require(isSuperAdmin(msg.sender), "Error: Not Super admin");
         checker = future_checker;
     }
     
     function approveWallet(address _wallet) public {
-        require(isAdmin(msg.sender), "!admin");
+        require(isAdmin(msg.sender), "Error: Not admin");
         wallets[_wallet] = true;
         
         emit ApproveWallet(_wallet);
     }
 
     function batchApproveWallets(address[] memory _wallets) public {
-        require(isAdmin(msg.sender), "!admin");
+        require(isAdmin(msg.sender), "Error: Not admin");
         for(uint i = 0; i < _wallets.length; i++) {
             wallets[_wallets[i]] = true;
             emit ApproveWallet(_wallets[i]);
@@ -70,7 +70,7 @@ contract SmartWalletWhitelistV2 {
     }
 
     function revokeWallet(address _wallet) external {
-        require(isAdmin(msg.sender), "!admin");
+        require(isAdmin(msg.sender), "Error: Not admin");
         wallets[_wallet] = false;
         
         emit RevokeWallet(_wallet);
